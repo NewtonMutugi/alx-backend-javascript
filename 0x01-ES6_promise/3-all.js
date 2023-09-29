fdf737bfabdc7558657d484575b2e6406b2a5795
NewtonMutugi <newtonmut197@gmail.com>
2023-09-30 01:32:06 +0300

    Modified 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,16 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  return new Promise(
-    (resolve) => {
-      const uploadPhotoPromise = uploadPhoto();
-      const createUserPromise = createUser();
-      resolve([uploadPhotoPromise, createUserPromise]);
-    },
-  ).then(
-    (values) => {
-      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-    },
-  ).catch(
-    () => console.log('Signup system offline'),
-  );
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then(
+      (values) => {
+        console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+      },
+    )
+    .catch(
+      () => console.log('Signup system offline'),
+    );
 }
 
 export default handleProfileSignup;
