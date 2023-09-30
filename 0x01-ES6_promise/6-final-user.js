d2d22bfb18efc42fe9c7a0198fedc3d8b5051661
NewtonMutugi <newtonmut197@gmail.com>
2023-09-30 14:28:18 +0300

    Added 6-final-user.js and 6-main.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstname, lastname, filename) {
+  return Promise
+    .allSettled([
+      signUpUser({ firstname, lastname }),
+      uploadPhoto(filename),
+    ])
+    .then(
+      (values) => (
+        values.map((l) => ({
+          status: l.status,
+          value: l.status === 'fulfilled' ? l.value : String(l.reason),
+        }))
+      ),
+    );
+}
