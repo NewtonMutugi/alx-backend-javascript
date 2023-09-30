eb0bb6ed06d5d841bb78317b079eea0a59b7c10d
NewtonMutugi <newtonmut197@gmail.com>
2023-09-30 17:02:49 +0300

    Added 2-hbtn_course.js and 2-main.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(myName) {
+    if (typeof myName !== 'string') throw new Error('Name must be a string');
+    this._name = myName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(myLength) {
+    if (typeof myLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = myLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(myStudents) {
+    if (!Array.isArray(myStudents)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (myStudents.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = myStudents;
+  }
+}
