1f61c50fdcdcf5c852f019acc86bbd94ba9ac9d0
NewtonMutugi <newtonmut197@gmail.com>
2023-09-30 18:18:31 +0300

    Modified 2-hbtn_course.js -

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,44 +1,42 @@
 class HolbertonCourse {
   constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
-    this._length = length;
-    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(myName) {
-    if (typeof myName !== 'string') {
-      throw new TypeError('Name must be a string');
+  set length(mylength) {
+    if (typeof mylength !== 'number') {
+      throw new TypeError('Length must be a number');
     }
-    this._name = myName;
+    this._length = mylength;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(myLength) {
-    if (typeof myLength !== 'number') {
-      throw new TypeError('Length must be a number');
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an Array');
     }
-    this._length = myLength;
   }
 
   get students() {
     return this._students;
-  }
-
-  set students(myStudents) {
-    if (!(myStudents instanceof Array)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    if (myStudents.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = myStudents;
   }
 }
 
