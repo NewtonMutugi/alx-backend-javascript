d301bdb609e2f96d25183899825c1c5f1d59c162
NewtonMutugi <newtonmut197@gmail.com>
2023-10-04 06:35:34 +0000

    Added 7-has_array_values.s

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,7 @@
 function hasValuesFromArray(set, arr) {
-    if (set instanceof Set && arr instanceof Array) {
-        return arr.every((value) => set.has(value));
-    }
+  if (set instanceof Set && arr instanceof Array) {
+    return arr.every((value) => set.has(value));
+  }
 }
 
 export default hasValuesFromArray;
