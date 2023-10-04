d92b9ab2956d4978e40e49135fe276360dbd541c
NewtonMutugi <newtonmut197@gmail.com>
2023-10-04 22:36:03 +0300

    Modified 7 and 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
-function cleanSet (set, startString) {
-    const newString = [];
-    if (set instanceof Set && startString instanceof String) {
-        for (const item of set) {
-            if (item && item.startsWith(startString)) {
-                newString.push(item.slice(startString.length));
-            }
-        }
-        return myString.join('-');
+function cleanSet(set, startString) {
+  const newString = [];
+  if (set instanceof Set && startString instanceof String) {
+    for (const item of set) {
+      if (item && item.startsWith(startString)) {
+        newString.push(item.slice(startString.length));
+      }
     }
+    return myString.join('-');
+  }
 }
 
 export default cleanSet;
