228c504cfee1ec8fd1937af8e0539591ed8dfecc
NewtonMutugi <newtonmut197@gmail.com>
2023-11-25 05:50:38 +0300

    4-http.js: Added headers

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,6 +5,8 @@ const port = 1245;
 app
   .createServer((req, res) => {
     res.write('Hello Holberton School!');
+    res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 200;
     res.end();
   })
   .listen(port);
