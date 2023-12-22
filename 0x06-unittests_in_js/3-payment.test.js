10edaca43b8e73cc67ac110b3b34d1fdb7b55801
NewtonMutugi <newtonmut197@gmail.com>
2023-12-22 12:54:42 +0300

    3-payments: Added the modules

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,11 +5,11 @@ const { expect } = require('chai');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const spy = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.false;
-    expect(calculateNumberSpy.callCount).to.be.equal(0);
-    calculateNumberSpy.restore();
+    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.calculateNumber.callCount).to.be.equal(1);
+    spy.calculateNumber.restore();
   });
 });
