da52679b505793108df690de460db2aff6341afc
NewtonMutugi <newtonmut197@gmail.com>
2023-12-22 15:41:02 +0300

    10-api: Added extra tests

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -14,7 +14,13 @@ app.get('/available_payments', (req, res) => {
   });
 });
 app.post('/login', (req, res) => {
-  res.send(`Welcome ${req.body.userName}`);
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
 });
 
 app.listen(7865, () => console.log('API available on localhost port 7865'));
