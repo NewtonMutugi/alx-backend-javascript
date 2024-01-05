af37abaab63a3f508ea9b358fd8a2b7f9be005f0
NewtonMutugi <newtonmut197@gmail.com>
2024-01-05 12:21:56 +0300

    Task 1: Printing teachers

---
+++ 0x04-TypeScript/task_1/dist/js/main.js
@@ -0,0 +1,21 @@
+var teacher3 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName.charAt(0), ". ").concat(lastName);
+};
+console.log(printTeacher('John', 'Doe'));
+//# sourceMappingURL=main.js.map
